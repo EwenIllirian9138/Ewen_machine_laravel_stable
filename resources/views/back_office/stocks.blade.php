aaa83f49b3ac2cf48dc3fdb0b7cd8ed161696142
Ewen Billot <ewen.billot@Digital-Grenoble.local>
2018-01-17 15:59:15 +0100

    vÃ2

--- resources/views/back_office/stocks.blade.php
+++ resources/views/back_office/stocks.blade.php
@@ -4,4 +4,48 @@
     Gestion des stocks
 
 @stop
-
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Type</th>
+        <th scope="col">Nombre</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($coin_list as $type => $number)
+        <tr>
+            <td>{{$i++}}</td>
+            <td>{{$type/100}}€</td>
+            <td>
+                <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <button type="button" class="btn btn-outline-danger">
+                        -
+                    </button>
+                    <button type="button" class="btn btn-secondary">
+                        {{$number}}
+                    </button>
+                    <button type="button" class="btn btn-outline-success">
+                        +
+                    </button>
+                </div>
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+    <tfooter>
+        <form>
+            <td>
+                <button type="submit" class="btn btn-outline-success">
+                    add
+                </button>
+            </td>
+            <td class="form-group">
+                <input type="text" class="form-control" placeholder="Type">
+            </td>
+            <td class="form-group">
+                <input type="text" class="form-control" placeholder="Number">
+            </td>
+        </form>
+    </tfooter>
+</table>
